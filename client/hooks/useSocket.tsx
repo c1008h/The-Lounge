b392fee39f5fa75c9afb2f32c030893ff0bb60b3
Chris Hong <hongchris97@gmail.com>
2024-03-17 06:24:17 -0700

    deploy url

--- client/hooks/useSocket.tsx
+++ client/hooks/useSocket.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import io, { Socket } from 'socket.io-client';
 
-if (typeof process.env.NEXT_PUBLIC_DEPLOY === 'undefined' || typeof process.env.NEXT_PUBLIC_SOCKET_PORT === 'undefined') throw new Error('NEXT_PUBLIC_SOCKET_PORT is not defined');
-const PORT = process.env.NEXT_PUBLIC_DEPLOY || process.env.NEXT_PUBLIC_SOCKET_PORT 
+if (typeof process.env.NEXT_PUBLIC_DEPLOY === 'undefined') throw new Error('NEXT_PUBLIC_DEPLOY is not defined');
+const PORT = process.env.NEXT_PUBLIC_DEPLOY 
+// || process.env.NEXT_PUBLIC_SOCKET_PORT 
 
 export const useSocket = (): { socket: Socket | null } => {
   const [socket, setSocket] = useState<Socket | null>(null);
