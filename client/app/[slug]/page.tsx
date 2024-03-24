7f05941aae4b301f7a0b6cd9815fd3ddcec0efee
Chris Hong <hongchris97@gmail.com>
2024-03-23 22:53:31 -0700

    socket works to enter the same session room

--- client/app/[slug]/page.tsx
+++ client/app/[slug]/page.tsx
@@ -188,13 +188,13 @@ export default function Anon({ params }: { params: { slug: string } }) {
       </div>
       <div className="flex-1 overflow-y-auto p-4">
         <div className="flex flex-col space-y-2">
-          {participants && messages?.map((message, index) => {
+          {messages?.map((message, index) => {
             const senderIsTempUser = message.sender.uid === anonUser?.uid;
-            const senderDisplayName = senderIsTempUser ? "" : participants.find(p => p.uid === (message.sender.uid || message.sender))?.displayName || "Unknown";
+            // const senderDisplayName = senderIsTempUser ? "" : participants.find(p => p.uid === (message.sender.uid || message.sender))?.displayName || "Unknown";
             console.log('messages:',messages)
             // const senderDisplayName = senderIsTempUser ? "" : participants?.find(p => p.uid === message.sender)?.displayName || "Unknown";
             // const formattedTimestamp = message.timestamp.toDate().toLocaleString();
-            const formattedTimestamp = formatTimestamp(message.timestamp)
+            // const formattedTimestamp = formatTimestamp(message.timestamp)
             const isLastMessage = index === messages.length - 1;
 
             return (
@@ -204,7 +204,7 @@ export default function Anon({ params }: { params: { slug: string } }) {
                 ref={isLastMessage ? messagesEndRef : null} 
               >
                 <div className="flex flex-col max-w-3/4">
-                  <p className={`text-sm ${senderIsTempUser ? 'text-right' : 'text-left'}`}>{senderDisplayName}</p>
+                  <p className={`text-sm ${senderIsTempUser ? 'text-right' : 'text-left'}`}>{message.sender.displayName}</p>
                   <div className={`py-2 px-4 rounded-lg max-w-3/4 ${message.sender.uid === anonUser?.uid ? 'bg-blue-600 text-white' : 'bg-gray-300'}`}>
                     <p>{message.message}</p>
                     {/* <p>{formattedTimestamp}</p> */}
