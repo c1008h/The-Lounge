b693d6b974aad7050f5df0d8adf001deacc9e4a4
Chris Hong <hongchris97@gmail.com>
2024-03-27 03:57:07 -0700

    moved server into its own repo

--- server/routes/index.js
+++
@@ -1,6 +0,0 @@
-const route = require('express').Router();
-const apiRoutes = require('./api')
-
-route.use('/api', apiRoutes)
-
-module.exports = route;
--- server/utils/tempIdGenerator.js
+++
@@ -1,7 +0,0 @@
-const { v4: uuidv4 } = require('uuid');
-
-function createUniqueId() {
-    return uuidv4(); 
-}
-
-module.exports = { createUniqueId }
